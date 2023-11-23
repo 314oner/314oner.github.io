9cb0b70905b54080ca98d8638ed95f635384d7b5
314oner <314oner@smtp.com>
2023-11-23 09:40:00 +0300

    add business logic for static-preact-app

---
+++ apps/static-preact-app/src/pages/about.tsx
@@ -0,0 +1,15 @@
+//@ts-nocheck
+const { h } = preact;
+import htm from "https://cdnjs.cloudflare.com/ajax/libs/htm/3.1.1/htm.module.js";
+//components
+import { PersonalInfo } from '../components/personal-info';
+const html = htm.bind(h);
+
+
+const About = () => {
+  return html`
+    <${PersonalInfo} />
+`;
+};
+
+export default About;
