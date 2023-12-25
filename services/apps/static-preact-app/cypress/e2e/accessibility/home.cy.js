8810ec6e89e389f92573d82d46b85535dafec1b8
314oner <314oner@smtp.com>
2023-12-25 10:54:26 +0300

    Add test case WCAG 2.0 Level A and AA button

--- services/apps/static-preact-app/cypress/e2e/accessibility/home.cy.js
+++ services/apps/static-preact-app/cypress/e2e/accessibility/home.cy.js
@@ -2,5 +2,11 @@ describe('A11y', () => {
   it('Главная страница отвечает требованиям доступности', () => {
     cy.injectAxe();
     cy.checkPageA11y('/');
+    cy.checkA11y("button", {
+      runOnly: {
+        type: "tag",
+        values: ["wcag2a", "wcag2aa"],
+      },
+    });
   });
 });
